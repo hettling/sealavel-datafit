84582c50bc3faaa6ec57714b213f340be3f4c6dd
hettling <hannes.hettling@gmail.com>
2016-02-25 17:15:17 +0100

    adding tabs

--- results/make-result-table.sh
+++ results/make-result-table.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 (printf 'compaction\t' \
-	&& echo `head -1 sealevel-comp-min.tsv` \
+	&& (echo `head -1 sealevel-comp-min.tsv` | tr " " "\t") \
 	&& printf "min\t" && tail -1 sealevel-comp-min.tsv \
 	&& printf "avg\t" && tail -1 sealevel-comp-avg.tsv \
 	&& printf "max\t" && tail -1 sealevel-comp-max.tsv) \
